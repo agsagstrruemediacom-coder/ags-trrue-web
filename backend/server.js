4e9d69c8a4276b9ccb673fdafc2aebadcfe8de9f
AGS TRRUE MEDIA <ags.agstrruemedia.com@gmail.com>
2026-07-14 17:42:51 +0530

    Changed SMTP port to 587

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,14 @@ app.post("/enquiry", async (req, res) => {
 
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
 
     const mailOptions = {
