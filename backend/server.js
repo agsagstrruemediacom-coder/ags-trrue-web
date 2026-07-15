5ffed300b35fb0cc649de027228c4d65d4b90586
AGS TRRUE MEDIA <ags.agstrruemedia.com@gmail.com>
2026-07-15 10:11:25 +0530

    Integrated EmailJS enquiry form

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,14 @@ app.post("/enquiry", async (req, res) => {
 
 
     const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS
     }
 });
-
 
     const mailOptions = {
         from: process.env.EMAIL_USER,
